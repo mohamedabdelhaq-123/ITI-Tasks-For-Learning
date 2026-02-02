2c3a84f7995f5ffb72c7d3ab96cac78782f72a90
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-02-02 15:57:10 +0200

    Add Angular userlists & todolist

---
+++ Java/Library Management System/Entities/LibraryItem.java
@@ -0,0 +1,32 @@
+package Entities;
+abstract public class LibraryItem {
+    // attributes
+    protected int id;
+    protected String title;
+
+    // access specifiers ,,modifiers
+
+    // getters
+     public int getId()          // Don't repeat yourself
+     {
+        return this.id;
+     }
+     public String getTitle()
+     {
+        return this.title;
+     }
+
+     // setters
+     public void setId(int id)
+     {
+            this.id=id;
+     }
+     public void setTitle(String title)
+     {
+            this.title=title;
+     }
+
+    // method
+   public abstract String getItemDetails() ;
+    
+}
